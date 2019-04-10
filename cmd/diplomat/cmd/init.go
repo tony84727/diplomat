052a21d7bf140d15f8eb12dc0b26532bcf7322a3
insufficientchocolate <tony84727@gmail.com>
2019-04-11 06:01:35 +0800

    add init command

---
+++ cmd/diplomat/cmd/init.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+)
+
+const exampleOutlineFile = `version: '1'
+settings:
+  chinese:
+    convert:
+      mode: t2s
+      from: zh-TW
+      to: zh-CN
+  copy:
+  - from: en
+    to: fr
+fragments:
+  admin:
+    description: translations for admin page
+    translations:
+      admin:
+        zh-TW: 管理員
+        en: Admin
+output:
+  fragments:
+  - type: js
+    name: "{{.Locale}}.{{.FragmentName}}.js"`
+
+var (
+	outline string
+	initCmd  = &cobra.Command{
+		Use: "init",
+		Short: "init diplomat project",
+		Run: func(cmd *cobra.Command, args []string) {
+			var projectDir string
+			if len(args) >= 0 {
+				projectDir = args[0]
+			} else {
+				var err error
+				projectDir, err = os.Getwd()
+				if err != nil {
+					fmt.Println("cannot get current working directory", err)
+					return
+				}
+			}
+			outlinePath := filepath.Join(projectDir, outline)
+			outlineDir := filepath.Dir(outlinePath)
+			if err := os.MkdirAll(outlineDir, 0755); err != nil {
+				fmt.Println(err)
+				return
+			}
+			if _, err := os.Stat(outlinePath); err != nil {
+				fmt.Println(err)
+				return
+			}
+			outlineFile, err := os.Create(outlinePath)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer outlineFile.Close()
+			if _, err := outlineFile.WriteString(exampleOutlineFile); err != nil {
+				fmt.Println(err)
+			}
+		},
+	}
+)
+
+func init() {
+	initCmd.Flags().StringVarP(&outline,"outline","o","outline.yaml","path to configuration file of diplomat")
+}
---
+++ cmd/diplomat/cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use: "diplomat",
+		Short: "diplomat is tool to manage i18n translation materials",
+	}
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
