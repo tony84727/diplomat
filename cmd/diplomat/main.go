e6c38cc577167d4373ea44c472cccfb9e5e30a4c
insufficientchocolate <tony84727@gmail.com>
2019-04-11 06:12:28 +0800

    replace subcommand by cobra

--- cmd/diplomat/main.go
+++ cmd/diplomat/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"context"
-	"flag"
-	"os"
-
-	"github.com/google/subcommands"
-)
+import "github.com/insufficientchocolate/diplomat/cmd/diplomat/cmd"
 
 func main() {
-	subcommands.Register(subcommands.HelpCommand(), "")
-	subcommands.Register(subcommands.FlagsCommand(), "")
-	subcommands.Register(&createCommand{}, "")
-	subcommands.Register(&generateCommand{}, "")
-	flag.Parse()
-	os.Exit(int(subcommands.Execute(context.Background(), os.Args)))
+	cmd.Execute()
 }
