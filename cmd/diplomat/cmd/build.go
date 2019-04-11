23198fb8f831831b88890bfd7b9340e54af3949c
insufficientchocolate <tony84727@gmail.com>
2019-04-12 06:12:51 +0800

    add watch as a flag

--- cmd/diplomat/cmd/build.go
+++ cmd/diplomat/cmd/build.go
@@ -11,38 +11,13 @@ import (
 )
 
 var (
+	watch bool
 	buildCmd = &cobra.Command{
 		Use: "build",
 		Short: "build",
 		Run: func(cmd *cobra.Command, args []string) {
 			var projectDir string
-			if len(args) >= 0 {
-				projectDir = args[0]
-			} else {
-				var err error
-				projectDir, err = os.Getwd()
-				if err != nil {
-					fmt.Println("cannot get current working directory", err)
-					return
-				}
-			}
-			d, err := diplomat.NewDiplomatForDirectory(projectDir)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(-1)
-			}
-			if err := d.Output("out"); err != nil {
-				fmt.Println(err)
-				os.Exit(-1)
-			}
-		},
-	}
-	watchCmd = &cobra.Command{
-		Use: "watch",
-		Short: "watch",
-		Run: func(cmd *cobra.Command, args []string) {
-			var projectDir string
-			if len(args) >= 0 {
+			if len(args) > 0 {
 				projectDir = args[0]
 			} else {
 				var err error
@@ -53,16 +28,33 @@ var (
 				}
 			}
 			outDir := filepath.Join(projectDir, "out")
-			d, errChan := diplomat.NewDiplomatWatchDirectory(projectDir)
-			go func() {
-				for e := range errChan {
-					log.Println("error:", e)
+			if watch {
+				d, errChan := diplomat.NewDiplomatWatchDirectory(projectDir)
+				go func() {
+					for e := range errChan {
+						log.Println("error:", e)
+					}
+				}()
+				go d.Watch(outDir)
+				quit := make(chan os.Signal, 1)
+				signal.Notify(quit, os.Interrupt)
+				<-quit
+			} else {
+				d, err := diplomat.NewDiplomatForDirectory(projectDir)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(-1)
 				}
-			}()
-			go d.Watch(outDir)
-			quit := make(chan os.Signal, 1)
-			signal.Notify(quit, os.Interrupt)
-			<-quit
+				if err := d.Output(outDir); err != nil {
+					fmt.Println(err)
+					os.Exit(-1)
+				}
+			}
+
 		},
 	}
 )
+
+func init() {
+	buildCmd.Flags().BoolVar(&watch, "watch",false,"watch changes")
+}
