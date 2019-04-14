06d178e9cbb458207598cfb0100f4ef65fd7eb66
insufficientchocolate <tony84727@gmail.com>
2019-04-14 17:38:00 +0800

    add interfaces for config

---
+++ pkg/data/configuration.go
@@ -0,0 +1,27 @@
+package data
+
+type Preprocessor interface {
+	GetType() string
+	GetOptions() interface{}
+}
+
+type Selector string
+
+type TemplateOption interface {
+	GetFilename() string
+}
+
+type Template interface {
+	GetType() string
+	GetOptions() TemplateOption
+}
+
+type Output interface {
+	GetSelectors() []Selector
+	GetTemplates() []Template
+}
+
+type Configuration interface {
+	GetPreprocessors() []Preprocessor
+	GetOutputs() []Output
+}
