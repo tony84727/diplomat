99fe2e734e0d61d1fd67ac9c9d6807f96e4949ee
insufficientchocolate <tony84727@gmail.com>
2019-04-14 19:19:05 +0800

    add configuration parser interface

--- pkg/parser/interface.go
+++ pkg/parser/interface.go
@@ -5,3 +5,7 @@ import "github.com/insufficientchocolate/diplomat/pkg/data"
 type TranslationParser interface {
 	GetTranslation() (data.Translation,error)
 }
+
+type ConfigurationParser interface {
+	GetConfiguration() (data.Configuration,error)
+}
