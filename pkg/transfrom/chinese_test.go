f75ac2ed5ac28ded226c62b8fb94ffc41ab1962d
insufficientchocolate <tony84727@gmail.com>
2019-04-15 06:20:45 +0800

    add test

---
+++ pkg/transfrom/chinese_test.go
@@ -0,0 +1,40 @@
+package transfrom
+
+import (
+	"github.com/insufficientchocolate/diplomat/pkg/data"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ChineseTransformerTestSuite struct {
+	suite.Suite
+}
+
+func (c ChineseTransformerTestSuite) TestTransform() {
+	transformer := NewChineseTransformer(ChineseTransformerOption{
+		mode: TranditionalToSimplified,
+		from: "zh-TW",
+		to:"zh-CN",
+	})
+	translations := data.NewTranslation("")
+	hello := data.NewTranslation("hello")
+	translations.AddChild(hello)
+	helloZhTW := data.NewTranslation("zh-TW")
+	helloZhTW.SetText("學問")
+	hello.AddChild(helloZhTW)
+	c.Require().NoError(transformer.Transform(translations))
+	exist := false
+	walker := data.NewTranslationWalker(translations)
+	walker.ForEachTextNode(func(path string, textNode data.Translation) error {
+		if path == ".hello.zh-CN" {
+			c.Equal("学问", *textNode.GetText())
+			exist = true
+		}
+		return nil
+	})
+	c.True(exist)
+}
+
+func TestChineseTransformer(t *testing.T) {
+	suite.Run(t, &ChineseTransformerTestSuite{})
+}
