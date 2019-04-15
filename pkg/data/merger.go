aca62b7137932f39d560150029e3c01b26ce7330
insufficientchocolate <tony84727@gmail.com>
2019-04-16 06:18:03 +0800

    implement merger

--- pkg/data/merger.go
+++ pkg/data/merger.go
@@ -5,7 +5,20 @@ type TranslationMerger struct {
 }
 
 func (t TranslationMerger) Merge(other Translation) {
-	panic("implement me")
+	walker := NewTranslationWalker(other)
+	_ = walker.ForEachTextNode(func(paths []string, textNode Translation) error {
+		var current Translation = t
+		for _, segment := range paths[1:len(paths)-1] {
+			child, exist := current.GetChildren()[segment]
+			if !exist {
+				child = NewTranslation(segment)
+				current.AddChild(child)
+			}
+			current = child
+		}
+		current.AddChild(textNode)
+		return nil
+	})
 }
 
 func NewTranslationMerger(root Translation) *TranslationMerger {
