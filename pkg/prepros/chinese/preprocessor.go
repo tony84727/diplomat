4979a77466864adc06077960106032a365814572
insufficientchocolate <tony84727@gmail.com>
2019-04-18 06:43:46 +0800

    add chinese preprocessor

---
+++ pkg/prepros/chinese/preprocessor.go
@@ -0,0 +1,81 @@
+package chinese
+
+import (
+	"errors"
+	"fmt"
+	"github.com/insufficientchocolate/diplomat/pkg/data"
+	"github.com/siongui/gojianfan"
+)
+
+type TransformMode int
+
+const (
+	SimplifiedToTranditonal TransformMode = iota
+	TranditionalToSimplified
+)
+
+type Config struct {
+	Mode TransformMode
+	From string
+	To string
+}
+
+type Preprocessor struct {
+}
+
+func (Preprocessor) parseConfig(option interface{}) (*Config, error) {
+	m,ok := option.(map[interface{}]interface{})
+	if !ok {
+		return nil,fmt.Errorf("expect option to be a map, got %v", option)
+	}
+	mode, ok := m["mode"].(string)
+	if !ok {
+		return nil,errors.New("expecting mode option: s2t or t2s")
+	}
+	from, ok := m["from"].(string)
+	if !ok {
+		return nil, errors.New("expecting from option")
+	}
+	to, ok := m["to"].(string)
+	if !ok {
+		return nil,errors.New("expecting to option")
+	}
+	enumMode := SimplifiedToTranditonal
+	if mode == "t2s" {
+		enumMode = TranditionalToSimplified
+	}
+	return &Config{
+		Mode: enumMode,
+		From:from,
+		To:to,
+	},nil
+}
+
+func (Preprocessor) transform(config *Config,input string) string {
+	if config.Mode == SimplifiedToTranditonal {
+		return gojianfan.S2T(input)
+	}
+	return gojianfan.T2S(input)
+}
+
+func (p Preprocessor) Process(translation data.Translation, option interface{}) error {
+	config, err := p.parseConfig(option)
+	if err != nil {
+		return err
+	}
+	walker := data.NewTranslationWalker(translation)
+	return walker.ForEachTextNode(func(paths []string, textNode data.Translation) error {
+		if paths[len(paths) -1] == config.From {
+			keyNode := textNode.GetParent()
+			// ignore if "to" node already exists
+			if _, exist := keyNode.GetChildren()[config.To]; exist {
+				return nil
+			}
+			toNode := data.NewTranslation(config.To)
+			toNode.SetText(p.transform(config, *textNode.GetText()))
+			keyNode.AddChild(toNode)
+		}
+		return nil
+	})
+}
+
---
+++ pkg/prepros/chinese/preprocessor_test.go
@@ -0,0 +1,42 @@
+package chinese
+
+import (
+	"github.com/insufficientchocolate/diplomat/pkg/data"
+	"github.com/stretchr/testify/suite"
+	"strings"
+	"testing"
+)
+
+type PreprocessorTestSuite struct {
+	suite.Suite
+}
+
+func (p PreprocessorTestSuite) TestProcess() {
+	instance := &Preprocessor{}
+	root := data.NewTranslation("")
+	questionNode := data.NewTranslation("question")
+	root.AddChild(questionNode)
+	traditional := data.NewTranslation("zh-TW")
+	traditional.SetText("問題")
+	questionNode.AddChild(traditional)
+	p.NoError(instance.Process(root, map[interface{}]interface{}{
+		"mode": "t2s",
+		"from": "zh-TW",
+		"to": "zh-CN",
+	}))
+	keys := make([]string, 0)
+	walker := data.NewTranslationWalker(root)
+	p.NoError(walker.ForEachTextNode(func(paths []string, textNode data.Translation) error {
+		key :=  strings.Join(paths, ".")
+		keys = append(keys,key)
+		if key == ".question.zh-CN" {
+			p.Equal("问题", *textNode.GetText())
+		}
+		return nil
+	}))
+	p.ElementsMatch([]string{".question.zh-TW",".question.zh-CN"}, keys)
+}
+
+func TestPreprocessor(t *testing.T) {
+	suite.Run(t, &PreprocessorTestSuite{})
+}
