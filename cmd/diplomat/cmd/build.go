f3dce94655b13983cacebfda8d3ae3aa01d6e1d8
insufficientchocolate <tony84727@gmail.com>
2019-04-20 15:21:34 +0800

    naively implement build using new packages

--- cmd/diplomat/cmd/build.go
+++ cmd/diplomat/cmd/build.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/insufficientchocolate/diplomat"
+	"github.com/insufficientchocolate/diplomat/internal"
+	"github.com/insufficientchocolate/diplomat/pkg/data"
+	"github.com/insufficientchocolate/diplomat/pkg/parser/yaml"
+	"github.com/insufficientchocolate/diplomat/pkg/prepros"
 	"github.com/spf13/cobra"
-	"log"
+	"io/ioutil"
 	"os"
-	"os/signal"
-	"path/filepath"
+	"strings"
 )
 
 var (
@@ -24,33 +26,66 @@ var (
 				projectDir, err = os.Getwd()
 				if err != nil {
 					fmt.Println("cannot get current working directory", err)
-					return
+					os.Exit(1)
 				}
 			}
-			outDir := filepath.Join(projectDir, "out")
-			if watch {
-				d, errChan := diplomat.NewDiplomatWatchDirectory(projectDir)
-				go func() {
-					for e := range errChan {
-						log.Println("error:", e)
-					}
-				}()
-				go d.Watch(outDir)
-				quit := make(chan os.Signal, 1)
-				signal.Notify(quit, os.Interrupt)
-				<-quit
-			} else {
-				d, err := diplomat.NewDiplomatForDirectory(projectDir)
+			//outDir := filepath.Join(projectDir, "out")
+			sourceSet := data.NewFileSystemSourceSet(projectDir)
+			configFile, err := sourceSet.GetConfigurationFile()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			configData, err := ioutil.ReadFile(configFile)
+			configParser := yaml.NewConfigurationParser(configData)
+			config, err := configParser.GetConfiguration()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			preprocessorConfigs := config.GetPreprocessors()
+			preprocessorInstances := make([]internal.PreprocessorFunc,0,len(preprocessorConfigs))
+			// reverse order
+			for i := len(preprocessorConfigs) -1; i >= 0; i-- {
+				p := preprocessorConfigs[i]
+				if instance := prepros.Manager.Get(p.GetType()); instance != nil {
+					preprocessorInstances = append(preprocessorInstances, func(translation data.Translation) error {
+						return instance.Process(translation, p.GetOptions())
+					})
+				}
+			}
+			preprocessorPipeline := internal.ComposePreprocessorFunc(preprocessorInstances...)
+			allTranslation := data.NewTranslationMerger(data.NewTranslation(""))
+			translationFiles, err := sourceSet.GetTranslationFiles()
+			if err != nil {
+				os.Exit(1)
+			}
+			for _, t := range translationFiles {
+				content, err := ioutil.ReadFile(t)
 				if err != nil {
 					fmt.Println(err)
-					os.Exit(-1)
+					os.Exit(1)
 				}
-				if err := d.Output(outDir); err != nil {
+				parser := yaml.NewParser(content)
+				translation, err := parser.GetTranslation()
+				if err != nil {
 					fmt.Println(err)
-					os.Exit(-1)
+					os.Exit(1)
 				}
+				allTranslation.Merge(translation)
 			}
-
+			if err := preprocessorPipeline(allTranslation); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			walker := data.NewTranslationWalker(allTranslation)
+			if err := walker.ForEachTextNode(func(paths []string, textNode data.Translation) error {
+				fmt.Println(strings.Join(paths, ".") + " => " + *textNode.GetText())
+				return nil
+			}); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		},
 	}
 )
--- pkg/prepros/manager.go
+++ pkg/prepros/manager.go
@@ -13,6 +13,10 @@ func (m ManagerImpl) Registry(name string, instance Preprocessor) {
 	m.preprocessors[name] = instance
 }
 
+func (m ManagerImpl) Get(name string) Preprocessor {
+	return m.preprocessors[name]
+}
+
 func newManager() *ManagerImpl {
 	return &ManagerImpl{
 		preprocessors: make(map[string]Preprocessor),
