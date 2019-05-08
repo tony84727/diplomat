bd53dfed28e5e9c843442362cc90adf71211aaab
tony84727 <tony84727@gmail.com>
2019-05-09 07:07:24 +0800

    fix config command

--- cmd/diplomat/internal/config_navigator.go
+++ cmd/diplomat/internal/config_navigator.go
@@ -9,12 +9,16 @@ import (
 )
 
 type ConfigNavigator struct {
+	previous reflect.Value
 	current reflect.Value
 	currentType reflect.Type
+	valid bool
 }
 
 func NewConfigNavigator(config data.Configuration) *ConfigNavigator {
-	return &ConfigNavigator{current: reflect.ValueOf(config), currentType: reflect.TypeOf(config)}
+	navigator := &ConfigNavigator{valid:true}
+	navigator.setCurrent(reflect.ValueOf(config))
+	return navigator
 }
 
 func structFieldHint(structValue reflect.Value) string {
@@ -27,39 +31,44 @@ func structFieldHint(structValue reflect.Value) string {
 }
 
 func (c *ConfigNavigator) setCurrent(value reflect.Value) {
+	c.previous = c.current
 	c.current = value
+	if !c.current.IsValid() {
+		c.valid = false
+		return
+	}
 	c.currentType = value.Type()
 }
 
 func (c *ConfigNavigator) Get(paths ...string) (interface{}, error) {
 	i := 0
 	for i < len(paths) {
+		segment := paths[i]
 		switch c.currentType.Kind() {
 		case reflect.Ptr, reflect.Interface:
 			c.setCurrent(c.current.Elem())
 			continue
 		case reflect.Slice:
-			index,err := strconv.Atoi(paths[i])
+			index,err := strconv.Atoi(segment)
 			if err != nil {
-				return nil,fmt.Errorf(`%s is a slice. "%s" is not a valid integer`, strings.Join(paths[:i],"."), paths[i])
+				return nil,fmt.Errorf(`%s is a slice. "%s" is not a valid integer`, strings.Join(paths[:i],"."), segment)
 			}
 			c.setCurrent(c.current.Index(index))
 			i++
 		case reflect.Struct:
 			searcher := FieldSearcher{c.currentType}
-			index, ok := searcher.Search(paths[i])
+			index, ok := searcher.Search(segment)
 			if !ok {
-				return nil, fmt.Errorf("%s doesn't exist, possible values %s", paths[i], structFieldHint(c.current))
+				return nil, fmt.Errorf("%s doesn't exist, possible values %s", segment, structFieldHint(c.current))
 			}
 			c.setCurrent(c.current.FieldByIndex(index))
 			i++
 		case reflect.Map:
-			c.setCurrent(c.current.MapIndex(reflect.ValueOf(paths[i])))
+			c.setCurrent(c.current.MapIndex(reflect.ValueOf(segment)))
 			i++
-		default:
-			if i < len(paths)  {
-				return nil, fmt.Errorf("%s doesn't exist. current: %T", paths[i], c.current.Interface())
-			}
+		}
+		if !c.valid {
+			return nil, fmt.Errorf("%s doesn't exist. current: %v", segment, c.previous.Interface())
 		}
 	}
 
